0552e7f524b8155776c6f0d324954723799b4aef
Stefan Djokic <stefan.djokic@tnation.eu>
2019-03-19 14:53:41 +0100

    implemented modules, reworked routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,17 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from '../app/modules/home/pages/home/home.component';
 import { ProfileComponent } from '../app/modules/profile/pages/profile/profile.component';
 import { LoginRegistrationComponent } from '../app/modules/login-registration/pages/login-registration/login-registration.component';
-import { LandingPageComponent } from '../app/modules/home/pages/landing-page/landing-page.component';
 import { AddTrackComponent } from '../app/modules/profile/pages/add-track/add-track.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'tracks', loadChildren: './modules/tracks/tracks.module#TracksModule' },
   { path: 'artists', loadChildren: './modules/artist/artist.module#ArtistModule' },
-  { path: 'home', component: HomeComponent },
+  { path: 'home', loadChildren: './modules/home/home.module#HomeModule' },
   { path: 'profile', component: ProfileComponent },
   { path: 'login', component: LoginRegistrationComponent },
-  { path: 'home/logout', component: LandingPageComponent },
   { path: 'add', component: AddTrackComponent },
   { path: '**', redirectTo: 'home/logout' }
 
--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'logout', component: LandingPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
